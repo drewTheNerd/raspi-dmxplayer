363d19df954cae7761540b2d93bde1ad7e005645
drewTheNerd <63565940+drewTheNerd@users.noreply.github.com>
2021-05-07 17:39:03 -0500

    Delete dmx-base.js

--- dmx-base.js
+++
@@ -1,7 +0,0 @@
-const DMX = require('dmx');
-
-const dmx = new DMX();
-
-var universe = dmx.addUniverse('demo', 'enttec-open-usb-dmx', '/dev/ttyUSB0');
-
-universe.update({1: 255, 2: 255, 3: 255, 4: 255});
